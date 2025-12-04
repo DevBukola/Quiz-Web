08f8375babdf54df75115e98fec610c41ed3b058
DevKafayat <olubukola11th@gmail.com>
2025-12-04 12:59:10 +0100

    try fix deployment issue

--- src/pages/CategoriesPage/_partials/Courses/_partials/Card/Card.jsx
+++ src/pages/CategoriesPage/_partials/Courses/_partials/Card/Card.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import styles from './card.module.css'
-
+import styles from "./Card.module.css";
 function Card({ title, bgImage }) {
   return (
-      <div id={styles.course} style={{ backgroundImage: `url(${bgImage})` }}>
+    <div id={styles.course} style={{ backgroundImage: `url(${bgImage})` }}>
       <div className={styles.cardInfo}>
         <h3> {title} </h3>
       </div>
